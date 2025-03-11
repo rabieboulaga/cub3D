b3b4a0546328e27a24272304cf264258eadde19c
Rabia Boulaga <rboulaga@c3r5p1.1337.ma>
2025-03-11 03:13:55 +0000

    nothing good

--- data_extraction.c
+++ data_extraction.c
@@ -3,36 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   data_extraction.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabia <rabia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rboulaga <rboulaga@students.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 12:19:45 by rboulaga          #+#    #+#             */
-/*   Updated: 2025/03/03 04:32:44 by rabia            ###   ########.fr       */
+/*   Updated: 2025/03/07 21:35:29 by rboulaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// void	skip_empty_lines(char **str)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	while(str[i])
-// 	{
-// 		if (ft_strlen(str[i]) == 1 && str[i][0] == '\n')
-// 			j++;
-
-// 	}
-// }
-
-void	type_definition(t_map *map, char *line)
+int	my_cmp(char *s1, char *s2, size_t n)
 {
-	int i;
+	size_t			i;
 
 	i = 0;
-	if(ft_strncmp)
+	while (*s2 && *s2 == ' ')
+		s2++;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+
+void	type_definition(t_map *map)
+{
+	while (map->next)
+	{
+		if(!my_cmp("NO ", map->line, 3))
+			printf(" line = %s : all is good\n", map->line);
+		map = map->next;
+	}
 }
 
 void	get_elements(t_map *map, char *str)
@@ -52,7 +56,7 @@ void	get_elements(t_map *map, char *str)
 void	data_extraction(char *file, t_data *data, t_map *map)
 {
 	char *str;
-	
+
 	data->fd = open(file, O_RDONLY);
 	map->line = get_next_line(data->fd);
 	str = get_next_line(data->fd);
@@ -62,12 +66,13 @@ void	data_extraction(char *file, t_data *data, t_map *map)
 		str = get_next_line(data->fd);
 		get_elements(map, str);
 	}
+	type_definition(map);
 
-	while (map->next)
-	{
-		printf("%s\n", map->line);
-		map = map->next;
-	}
+	// while (map->next)
+	// {
+	// 	printf("%s\n", map->line);
+	// 	map = map->next;
+	// }
 	close(data->fd);
 }
 
@@ -83,14 +88,14 @@ void	data_extraction(char *file, t_data *data, t_map *map)
 	// 		data->map1[i] = get_next_line(data->fd);
 	// 	}
 	// 	data->map1[i] = '\0';
-	
+
 
 	// int	remove_newline(char **str)
 	// {
 	// 	int	i;
 	// 	int	j;
 	// 	int	len;
-	
+
 	// 	j = 0;
 	// 	len = 0;
 	// 	while (str && str[len])
@@ -106,3 +111,26 @@ void	data_extraction(char *file, t_data *data, t_map *map)
 	// 	}
 	// 	return (len);
 	// }
+
+
+
+// int	ft_strncmp(const char *s1, const char *s2, size_t n)
+// {
+// 	size_t			i;
+// 	unsigned char	*str1;
+// 	unsigned char	*str2;
+
+// 	str1 = (unsigned char *)s1;
+// 	str2 = (unsigned char *)s2;
+// 	i = 0;
+// 	j = 0;
+// 	while (str2[i] && str2[i] == ' ')
+// 		i++;
+// 	while (i < n && (s1[i + j] != '\0' || s2[i + j] != '\0'))
+// 	{
+// 		if (str1[i + j] != str2[i + j])
+// 			return (str1[i + j] - str2[i + j]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
